501d4ed2c27a88c168728a16efd3c25a66b48e80
Keeghan McGarry <keeghan_m@live.com>
2024-07-01 16:15:24 +0100

    Holding pages

---
+++ src/app/members/page.tsx
@@ -0,0 +1,12 @@
+import { auth } from '@/auth'
+
+export default async function Members(): Promise<JSX.Element> {
+  const session = await auth()
+  if (!session?.user?.id) return <h1>Log in to access this page.</h1>
+
+  return (
+    <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-center flex items-center gap-4">
+      Members List <div className="badge badge-warning">Coming Soon</div>
+    </h1>
+  )
+}
--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,3 +1,42 @@
-export function Membership(): JSX.Element {
-  return <h1>Coming Soon.</h1>
+import { eq } from 'drizzle-orm'
+import { auth } from '@/auth'
+import { db } from '@/db/db'
+import { memberData, users } from '@/db/schema'
+
+export default async function Membership(): Promise<JSX.Element> {
+  const session = await auth()
+  if (!session?.user?.id) return <h1>Log in to access this page.</h1>
+
+  const user = await db
+    .select({
+      memberNumber: memberData.memberNumber,
+      active: memberData.active_payment,
+      override: memberData.payment_override,
+      ecfRating: memberData.ecf_rating,
+      onlineRating: memberData.online_rating,
+      fideId: memberData.fide_id,
+      ecfNumber: memberData.ecf_number,
+      lichess: memberData.lichess_username,
+      chesscom: memberData.chesscom_username,
+      email: users.email,
+    })
+    .from(users)
+    .leftJoin(memberData, eq(users.id, memberData.userId))
+    .where(eq(users.id, session.user.id))
+    .then((result) => result[0])
+
+  if (!user) throw new Error('User not found')
+
+  return (
+    <div>
+      <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-center flex items-center gap-4">
+        My Membership
+        {user.active ?? user.override ? (
+          <div className="badge badge-success">Paid Up</div>
+        ) : (
+          <div className="badge badge-error">Unpaid</div>
+        )}
+      </h1>
+    </div>
+  )
 }
